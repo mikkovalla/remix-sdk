713e54913414985a2de2cc6dab8a120639503076
Mikko Vallaskivi <mikkovalla@users.noreply.github.com>
2024-04-19 13:57:00 +0300

    update packages

--- example/app/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import { useFlags } from 'launchdarkly-remix-sdk/client';
-
-export default function Index() {
-  const { 'dev-test-flag': devTestFlag } = useFlags();
-
-  return <>{devTestFlag ? `Welcome to Remix with LaunchDarkly!` : `Welcome to Remix!`}</>;
-}
--- example/types/globals.d.ts
+++
@@ -1,8 +0,0 @@
-import { LDFlagSet } from 'launchdarkly-js-client-sdk';
-
-declare global {
-  interface Window {
-    clientSideID: string;
-    ssrFlags: LDFlagSet;
-  }
-}
